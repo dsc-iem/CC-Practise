efddc1866a5b9d8a9a435264d425136282498394
7sparsh <saxena.sparsh2001@gmail.com>
2020-10-02 11:03:38 +0530

    Added Solution for Dynamic Array problem

---
+++ Dynamic Array/Sparsh_Saxena/dynamicArray_hackerrank.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl '\n'
+using namespace std;
+
+int main(){
+
+	ll n,q;
+	cin >> n >> q;
+    vector<ll>seqList[n];
+    ll lastAns = 0;
+    for(ll i=0;i<q;i++) {
+        ll ch,x,y;
+        cin >> ch >> x >> y;
+        ll pos = (x^lastAns)%n;
+        if(ch==1) {
+                seqList[pos].push_back(y);
+            }
+            else if(ch==2){
+                ll index = y%((ll)seqList[pos].size());
+                lastAns = seqList[pos][index];
+                cout << lastAns << endl;
+            }
+	}
+	return 0;
+}
+
+
